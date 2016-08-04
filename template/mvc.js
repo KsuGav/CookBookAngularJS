4bdc48297f6479594dfc555cbac49979450c8bb9
Ksu <Gaydamanchukksena@gmail.com>
2016-08-04 15:40:23 +0300

    add functions to controller

--- template/mvc.js
+++ template/mvc.js
@@ -3,44 +3,85 @@
  */
 var receiptes = [
 {
-    name: "soup",
-    ingridients: {1:'ffff',2:'ddddd',3:'ddddd'},
-    describe: "sssssssssss",
+    name: "Soup",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto aspernatur " +
+    "error est iusto labore possimus tenetur ut voluptatem voluptatum. Culpa excepturi non placeat quasi" +
+    " quia quod temporibus velit voluptates.",
+    photo:"img/soup.jpg",
     date: '2015-01-05'
 },
+    {
+        name: "Steak",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto aspernatur " +
+        "error est iusto labore possimus tenetur ut voluptatem voluptatum. Culpa excepturi non placeat quasi " +
+        "quia quod temporibus velit voluptates.",
+        photo:"img/steak.jpg",
+        date: '2015-07-12'
+    },
 {
-    name: "salad",
-    ingridients: {1:'uuuuu',2:'eeeee',3:'mmmmmm'},
-    describe: "rrrrrrrrrr",
+    name: "Salad",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto aspernatur " +
+    "error est iusto labore possimus tenetur ut voluptatem voluptatum. Culpa excepturi non placeat quasi " +
+    "quia quod temporibus velit voluptates.",
+    photo:"img/salad.jpeg",
     date: '2015-11-25'
 },
 {
-    name: "desert",
-    ingridients:{1:'ffff',2:'aaaaa',3:'zzzzz'},
-    describe: "gggggggggggg",
+    name: "Desert",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto aspernatur " +
+    "error est iusto labore possimus tenetur ut voluptatem voluptatum. Culpa excepturi non placeat quasi " +
+    "quia quod temporibus velit voluptates.",
+    photo:"img/dessert.jpg",
     date: '2016-04-15'
 }
     ];
-localStorage.setItem('receipt', JSON.stringify(receiptes)); // записати
-var book = JSON.parse(localStorage.getItem('receipt')); // отримати
+localStorage.setItem('receipt', JSON.stringify(receiptes));
+var book = JSON.parse(localStorage.getItem('receipt'));
 
-console.log(book[0].ingridients);
+//console.log(book);
 
 var app = angular.module('myApp', []);
 app.controller('myCtrl',function($scope){
     $scope.book = book;
     $scope.delDiv = function(e){
-        $(e.target).parent().parent().remove();
+        //$(e.target).parent().parent().remove();
+        $scope.book.splice(e,1);
+        console.log(book);
     };
-    $scope.editDiv = function(element,$event){
-        $($event).find('input').focus();
-    }
-    //$scope.addDiv =  function() {
-    //    $scope.book.push({name:$scope.todoName, ingridients:$scope.todoMobile, describe:$scope.todoDesc, date:$scope.todoDesc,done:false});
-    //    $scope.todoName = '';
-    //    $scope.todoMobile = '';
-    //    $scope.todoDesc = '';
-    //};
+    $scope.editDiv = function(e){//oткрывается для редактироания с текстом выбраного дива
+        $('#addEdit').show();
+        $scope.show = true;
+      $('#newName').val($scope.book[e].name);
+        $('#newDescription').val($scope.book[e].description);
+
+    };
+    $scope.saveEdit = function(e){ //сохраняется измененная инфа
+        $scope.addName = $('#newName').val();
+        $scope.addDescription = $('#newDescription').val();
+        $scope.addDate = new Date();
+        $scope.hide = true;
+        $scope.book[e]=({name:$scope.addName, description:$scope.addDescription, date:$scope.addDate, done:false});
+    };
+    $scope.saveDiv =  function() {
+        $scope.addName = $('#newName').val();
+        $scope.addDescription = $('#newDescription').val();
+        $scope.addDate = new Date();
+        $scope.hide = true;
+        $scope.book.push({name:$scope.addName, description:$scope.addDescription, date:$scope.addDate, done:false});
+        //$scope.add = function(){
+        //    var reader = new FileReader();
+        //    reader.onloadend = function() {
+        //        var res = this.result
+        //        return res ;
+        //    };
+        //    $scope.book.push({photo:res});
+        //}
+    };
+
     //for Edit
     //$scope.now = new Date();
-})
+});
+
+//как подключиться к отдельному файлу json
+//загрузить картинку в новый див
+//страница загружается с задержкой
